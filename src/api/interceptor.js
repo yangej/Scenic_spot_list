83ff262a9bc9e0eda0908cf43c69266893aa80a1
yangej <yangej0218@gmail.com>
2021-03-06 00:51:47 +0800

    cope with api error

--- src/api/interceptor.js
+++ src/api/interceptor.js
@@ -1,5 +1,7 @@
 export const setInterceptor = (axiosInstance) => {
     axiosInstance.interceptors.response.use((response) => {
-        return response.status === 200 && response.data;
+        return response.data;
+    }, (error) => {
+        return Promise.reject(error.message);
     });
 };
