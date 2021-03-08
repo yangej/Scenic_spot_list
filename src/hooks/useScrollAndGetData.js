5092922993c0ba0c941b2f191ed6d06489ea4768
yangej <yangej0218@gmail.com>
2021-03-08 11:34:33 +0800

    fix go-to-top issue when get more data

--- src/hooks/useScrollAndGetData.js
+++ src/hooks/useScrollAndGetData.js
@@ -3,21 +3,27 @@ import {debounce} from "../utils/debounce";
 
 const useScrollAndGetData = (getData, city) => {
     const spotCount = 30;
-    let getSpotFrom = 0;
-    let isDone = false;
     const [ spots, setSpots ] = useState([]);
+    const [ getSpotFrom, setGetSpotFrom ] = useState(0);
+    const [ isDone, setIsDone ] = useState(false);
 
     const handleScroll = async function (e) {
         const scrollingElement = e.target.scrollingElement;
         const isAtBottom = scrollingElement.clientHeight + scrollingElement.scrollTop > scrollingElement.scrollHeight - 10;
 
         if (isAtBottom && !isDone) {
-            getSpotFrom += spotCount;
-
-            let data = await getData.apply(null, [spotCount, getSpotFrom]);
-            data.length ? setSpots((spots) => spots.concat(data)) : isDone = true;
+            setGetSpotFrom( getSpotFrom + spotCount);
         }
     };
+
+    useEffect(() => {
+        getSpotFrom && (async function getMoreSpots () {
+            let data = await getData.apply(null, [spotCount, getSpotFrom]);
+            data.length ? setSpots((spots) => spots.concat(data)) : setIsDone(true);
+        })();
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [getSpotFrom]);
 
     useEffect(() => {
         (async function firstGetSpots () {
@@ -26,9 +32,7 @@ const useScrollAndGetData = (getData, city) => {
         })();
 
         const debouncedScrollHandler = debounce(handleScroll, 500);
-
         window.addEventListener('scroll', debouncedScrollHandler);
-
         return () => {
             window.removeEventListener('scroll', debouncedScrollHandler);
         }
