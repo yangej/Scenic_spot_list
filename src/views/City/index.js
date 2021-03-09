b983f910ed35d894aa7eb1142b800b6a007d9d77
yangej <yangej0218@gmail.com>
2021-03-09 19:26:44 +0800

    remove unnecessary import

--- src/views/City/index.js
+++ src/views/City/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { apiExecutor } from "../../api";
 import { useDispatch } from "react-redux";
 import { useParams, withRouter } from "react-router";
